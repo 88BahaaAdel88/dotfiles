9b690c50a3fa87d54f4d307fcee1fa68777caa3b
Bahaa <bahaaadel47880@gmail.com>
2024-06-15 00:25:22 +0300

    first commit of dotfiles

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
+vim.cmd("filetype on")
+vim.cmd("set relativenumber")
+vim.g.mapleader = " "
+vim.cmd("set clipboard+=unnamedplus")
